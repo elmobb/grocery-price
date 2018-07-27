b87a7598a8c9b8be6d01d8c8f512d1ba71655449
elmobb <41227515+elmobb@users.noreply.github.com>
2018-07-27 22:52:59 +0800

    Add measures to avoid getting banned.

--- supermarket-crawler/settings.py
+++ supermarket-crawler/settings.py
@@ -10,3 +10,11 @@ FEED_URI = "test.json"
 ITEM_PIPELINES = {
     "supermarket-crawler.item_pipelines.PricePipeline": 300
 }
+
+DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
+}
+
+COOKIES_ENABLED = False
+DOWNLOAD_DELAY = 2
