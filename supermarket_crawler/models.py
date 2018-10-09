d7fa8f14caa323f25e6e6f3de05e176b8043e5f4
elmobb <41227515+elmobb@users.noreply.github.com>
2018-10-10 07:17:54 +0800

    Be more merciful to crawlers by accepting null values for most fields when writing to database.

--- supermarket_crawler/models.py
+++ supermarket_crawler/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, UniqueConstraint, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
@@ -8,38 +8,38 @@ Base = declarative_base()
 class CrawlerStats(Base):
     __tablename__ = "crawler_stats"
     id = Column(Integer, primary_key=True)
-    downloader_request_bytes = Column(Integer, nullable=True)
-    downloader_request_count = Column(Integer, nullable=True)
-    downloader_response_bytes = Column(Integer, nullable=True)
-    downloader_response_count = Column(Integer, nullable=True)
-    dupefilter_filtered = Column(Integer, nullable=True)
-    finish_reason = Column(String(100), nullable=False)
-    finish_time = Column(DateTime, nullable=False)
-    item_scraped_count = Column(Integer, nullable=True)
-    log_count_debug = Column(Integer, nullable=True)
-    log_count_info = Column(Integer, nullable=True)
-    memusage_max = Column(Integer, nullable=True)
-    memusage_startup = Column(Integer, nullable=True)
-    request_depth_max = Column(Integer, nullable=True)
-    response_received_count = Column(Integer, nullable=True)
-    scheduler_dequeued = Column(Integer, nullable=True)
-    scheduler_dequeued_memory = Column(Integer, nullable=True)
-    scheduler_enqueued = Column(Integer, nullable=True)
-    scheduler_enqueued_memory = Column(Integer, nullable=True)
-    start_time = Column(DateTime, nullable=False)
+    downloader_request_bytes = Column(Integer)
+    downloader_request_count = Column(Integer)
+    downloader_response_bytes = Column(Integer)
+    downloader_response_count = Column(Integer)
+    dupefilter_filtered = Column(Integer)
+    finish_reason = Column(String(100))
+    finish_time = Column(DateTime)
+    item_scraped_count = Column(Integer)
+    log_count_debug = Column(Integer)
+    log_count_info = Column(Integer)
+    memusage_max = Column(Integer)
+    memusage_startup = Column(Integer)
+    request_depth_max = Column(Integer)
+    response_received_count = Column(Integer)
+    scheduler_dequeued = Column(Integer)
+    scheduler_dequeued_memory = Column(Integer)
+    scheduler_enqueued = Column(Integer)
+    scheduler_enqueued_memory = Column(Integer)
+    start_time = Column(DateTime)
 
 
 class Product(Base):
     __tablename__ = "product"
     __table_args__ = (UniqueConstraint("shop", "sku", "update_time", name="shop_sku"),)
     id = Column(Integer, primary_key=True)
-    shop = Column(String(100), nullable=False)
-    sku = Column(String(100), nullable=False)
-    brand_name = Column(String(1000), nullable=True)
-    name = Column(String(1000), nullable=False)
-    uom = Column(String(100), nullable=False)
-    url = Column(String(1000), nullable=False)
-    update_time = Column(DateTime, nullable=False)
+    shop = Column(String(100))
+    sku = Column(String(100))
+    brand_name = Column(String(1000))
+    name = Column(String(1000))
+    uom = Column(String(100))
+    url = Column(String(1000))
+    update_time = Column(DateTime)
 
     prices = relationship("Price")
 
@@ -48,9 +48,9 @@ class Price(Base):
     __tablename__ = "price"
     __table_args__ = (UniqueConstraint("product_id", "update_time", name="product_id_update_time"),)
     id = Column(Integer, primary_key=True)
-    price = Column(Float, nullable=False)
-    currency = Column(String(100), nullable=False)
-    update_time = Column(DateTime, nullable=False)
+    price = Column(Float)
+    currency = Column(String(100))
+    update_time = Column(DateTime)
 
     product_id = Column(Integer, ForeignKey("product.id"))
 
