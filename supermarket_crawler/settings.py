3c3fdc82722efcbcc3ef75b33a17b90380f4212d
elmobb <41227515+elmobb@users.noreply.github.com>
2018-10-10 06:40:34 +0800

    Show only INFO level logging in console.

--- supermarket_crawler/settings.py
+++ supermarket_crawler/settings.py
@@ -21,6 +21,7 @@ FEED_URI = os.environ.get("FEED_URI")
 ITEM_PIPELINES = {
     "supermarket_crawler.item_pipelines.PricePipeline": 300
 }
-LOG_FILE = "log.txt"
+LOG_STDOUT = True
+LOG_LEVEL = "INFO"
 PARK_N_SHOP_START_URLS = "https://www.parknshop.com/zh-hk/beverages-wine-spirits/lc/040000|https://www.parknshop.com/en/groceries/lc/020000|https://www.parknshop.com/en/biscuits-snacks-confectionery/lc/030000|https://www.parknshop.com/zh-hk/household/lc/200000|https://www.parknshop.com/zh-hk/baby-care/lc/080000|https://www.parknshop.com/zh-hk/health-beauty-care/lc/090000|https://www.parknshop.com/zh-hk/frozen-food/lc/060000|https://www.parknshop.com/zh-hk/fresh-food/lc/070000https://www.parknshop.com/zh-hk/breakfast-bakery/lc/010000|https://www.parknshop.com/zh-hk/dairy-chilled-eggs/lc/050000|https://www.parknshop.com/zh-hk/pet-care/lc/210000"
 SPIDER_MODULES = ["supermarket_crawler.spiders"]
