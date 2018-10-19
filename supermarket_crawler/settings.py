9eb5ac8534a321d1adc1eba96e85f4294d7f39a5
elmobb <41227515+elmobb@users.noreply.github.com>
2018-10-19 21:45:35 +0800

    Fix Scrapy setting for proper deployment.

--- supermarket_crawler/settings.py
+++ supermarket_crawler/settings.py
@@ -1,10 +1,8 @@
-from config import Config
-
-config = Config()
+import os
 
 BOT_NAME = "price"
 COOKIES_ENABLED = False
-DATABASE_URI = config.DATABASE_URI
+DATABASE_URI = os.environ.get("DATABASE_URI")
 DOWNLOAD_DELAY = 2
 DOWNLOADER_MIDDLEWARES = {
     "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
@@ -19,7 +17,7 @@ FEED_EXPORT_ENCODING = "utf8"
 FEED_STORAGES = {
     "sftp": "scrapy_feedexporter_sftp.SFTPFeedStorage"
 }
-FEED_URI = config.FEED_URI
+FEED_URI = os.environ.get("FEED_URI")
 LOG_STDOUT = True
 LOG_LEVEL = "INFO"
 PARK_N_SHOP_START_URLS = "https://www.parknshop.com/zh-hk/beverages-wine-spirits/lc/040000|https://www.parknshop.com/en/groceries/lc/020000|https://www.parknshop.com/en/biscuits-snacks-confectionery/lc/030000|https://www.parknshop.com/zh-hk/household/lc/200000|https://www.parknshop.com/zh-hk/baby-care/lc/080000|https://www.parknshop.com/zh-hk/health-beauty-care/lc/090000|https://www.parknshop.com/zh-hk/frozen-food/lc/060000|https://www.parknshop.com/zh-hk/fresh-food/lc/070000https://www.parknshop.com/zh-hk/breakfast-bakery/lc/010000|https://www.parknshop.com/zh-hk/dairy-chilled-eggs/lc/050000|https://www.parknshop.com/zh-hk/pet-care/lc/210000"
