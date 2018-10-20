97057c78d210bb51e04ec7b191d1e556c6319379
elmobb <41227515+elmobb@users.noreply.github.com>
2018-10-20 21:42:22 +0800

    Remove item pipeline to upload items to database.

--- supermarket_crawler/settings.py
+++ supermarket_crawler/settings.py
@@ -20,9 +20,6 @@ FEED_STORAGES = {
     "sftp": "scrapy_feedexporter_sftp.SFTPFeedStorage"
 }
 FEED_URI = config.FEED_URI
-ITEM_PIPELINES = {
-    "supermarket_crawler.item_pipelines.DatabasePipeline": 300
-}
 LOG_STDOUT = True
 LOG_LEVEL = "INFO"
 PARK_N_SHOP_START_URLS = "https://www.parknshop.com/zh-hk/beverages-wine-spirits/lc/040000|https://www.parknshop.com/en/groceries/lc/020000|https://www.parknshop.com/en/biscuits-snacks-confectionery/lc/030000|https://www.parknshop.com/zh-hk/household/lc/200000|https://www.parknshop.com/zh-hk/baby-care/lc/080000|https://www.parknshop.com/zh-hk/health-beauty-care/lc/090000|https://www.parknshop.com/zh-hk/frozen-food/lc/060000|https://www.parknshop.com/zh-hk/fresh-food/lc/070000https://www.parknshop.com/zh-hk/breakfast-bakery/lc/010000|https://www.parknshop.com/zh-hk/dairy-chilled-eggs/lc/050000|https://www.parknshop.com/zh-hk/pet-care/lc/210000"
