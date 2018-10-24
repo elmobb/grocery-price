46d9a9ab9e24c0afd08dc4b23efe84e6eb893bde
elmobb <elmobb111@gmail.com>
2018-10-25 00:06:03 +0800

    Enable spider stats logger.

--- grocery_price/settings.py
+++ grocery_price/settings.py
@@ -14,6 +14,12 @@ DOWNLOADER_MIDDLEWARES = {
     "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
 }
 
+EXTENSIONS = {
+    "grocery_price.extensions.StatsLogger": 400
+}
+
 FEED_STORAGES = {
     "sftp": "scrapy_feedexporter_sftp.SFTPFeedStorage"
 }
+
+DATABASE_URI = "sqlite://"
