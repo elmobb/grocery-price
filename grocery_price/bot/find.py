f42a7923b0e17acaeaa2be49bb608578eb2668ec
elmobb <elmobb111@gmail.com>
2018-10-30 16:14:55 +0800

    Refactor bot's find module.

--- grocery_price/bot/find.py
+++ grocery_price/bot/find.py
@@ -6,86 +6,92 @@ from grocery_price.bot.utils import find_minimum_price, find_products, get_filte
 KEYWORDS, BRAND_NAME, PRODUCT_NAME, UOM, SHOP, ACTION = range(6)
 
 
-def find(bot, update, chat_data, args):
+def find_products_by_chat_data(chat_data):
+    return find_products(
+        keywords=chat_data.get("keywords"),
+        shop=chat_data.get("shop"),
+        brand_name=chat_data.get("brand_name"),
+        name=chat_data.get("name"),
+        uom=chat_data.get("uom")
+    )
+
+
+def start(bot, update, chat_data, args):
     chat_data.clear()  # Prevent this find being affected by previous chats.
     chat_data["keywords"] = args
-    products = find_products(keywords=chat_data["keywords"])
+    products = find_products_by_chat_data(chat_data=chat_data)
 
     # No products found.
     if len(products) == 0:
         update.message.reply_text(text="Oops! No products found.")
         return ConversationHandler.END
 
-    # Get unique brand names.
-    brand_names = get_filter_keys(products=products, key="brand_name")
-
     # Send welcome message.
     update.message.reply_text(
         "Hi! Are you looking for a product?\nSend /cancel to cancel.",
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(f"{i[0]} ({i[1]})", callback_data=i[0])] for i in brand_names
+            [InlineKeyboardButton(f"{i[0]} ({i[1]})", callback_data=i[0])] for i in get_filter_keys(
+                products=products,
+                key="brand_name"
+            )
         ])
     )
 
     return BRAND_NAME
 
 
-def get_filter(step):
+def filter(by):
     """Get filter state function. Filter state filters out products that fulfill criteria.
     """
-    state = [BRAND_NAME, PRODUCT_NAME, UOM, SHOP]
+    states = [BRAND_NAME, PRODUCT_NAME, UOM, SHOP]
     keys = ["brand_name", "name", "uom", "shop"]
-    has_next_state = (step + 1 < len(state))
+    step = [i for i, v in enumerate(keys) if v == by][0]
+    has_next_state = (step + 1 < len(states))
 
-    def filter_(bot, update, chat_data):
+    def func(bot, update, chat_data):
+        if not has_next_state:
+            return select_action(bot=bot, update=update, chat_data=chat_data)
+
         query = update.callback_query
-
         chat_data[keys[step]] = query.data
+        products = find_products_by_chat_data(chat_data=chat_data)
 
-        # Get products fulfilled selection in previous state.
-        products = find_products(
-            keywords=chat_data.get("keywords"),
-            shop=chat_data.get("shop"),
-            brand_name=chat_data.get("brand_name"),
-            name=chat_data.get("name"),
-            uom=chat_data.get("uom")
-        )
-
-        if has_next_state:
-
-            # Get filter keys.
-            values = get_filter_keys(products=products, key=keys[step + 1])
-
-            # Show product filter key buttons.
-            bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
-            bot.send_message(
-                chat_id=query.message.chat_id,
-                text=query.message.text,
-                parse_mode="Markdown",
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton(f"{i[0]} ({i[1]})", callback_data=i[0])] for i in values
-                ])
-            )
-
-            return state[step + 1]
-
-        chat_data["product"] = products[0]
-
-        # Show action buttons.
+        # Show product filter key buttons.
         bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
         bot.send_message(
             chat_id=query.message.chat_id,
-            text=f"What do you want to know about *{chat_data['product'].name} ({chat_data['product'].uom})* in *{chat_data['product'].shop}* (sku: {chat_data['product'].sku})?",
+            text=query.message.text,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("View", callback_data="view", url=chat_data["product"].url)],
-                [InlineKeyboardButton("Low", callback_data="low")]
+                [InlineKeyboardButton(f"{i[0]} ({i[1]})", callback_data=i[0])] for i in get_filter_keys(
+                    products=products,
+                    key=keys[step + 1]
+                )
             ])
         )
 
-        return ACTION
+        return states[step + 1]
 
-    return filter_
+    return func
+
+
+def select_action(bot, update, chat_data):
+    query = update.callback_query
+    chat_data["product"] = find_products_by_chat_data(chat_data=chat_data)[0]
+
+    # Show action buttons.
+    bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
+    bot.send_message(
+        chat_id=query.message.chat_id,
+        text=f"What do you want to know about *{chat_data['product'].name} ({chat_data['product'].uom})* in *{chat_data['product'].shop}* (sku: {chat_data['product'].sku})?",
+        parse_mode="Markdown",
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("View", callback_data="view", url=chat_data["product"].url)],
+            [InlineKeyboardButton("Low", callback_data="low")]
+        ])
+    )
+
+    return ACTION
 
 
 def action(bot, update, chat_data):
@@ -104,8 +110,11 @@ def low(bot, update, chat_data):
     query = update.callback_query
 
     # Show low price text message.
-    min_prices = find_minimum_price(shop=chat_data['product'].shop, sku=chat_data['product'].sku,
-                                    days=[1, 7, 2 * 7, 13 * 7, 52 * 7])
+    min_prices = find_minimum_price(
+        shop=chat_data['product'].shop,
+        sku=chat_data['product'].sku,
+        days=[1, 7, 2 * 7, 13 * 7, 52 * 7]
+    )
     bot.delete_message(chat_id=query.message.chat_id, message_id=query.message.message_id)
     bot.send_message(
         chat_id=query.message.chat_id,
@@ -136,24 +145,24 @@ def cancel(bot, update):
 
 find_handler = ConversationHandler(
     entry_points=[
-        CommandHandler("find", find, pass_chat_data=True, pass_args=True)
+        CommandHandler("find", start, pass_chat_data=True, pass_args=True)
     ],
     states={
         # Select a product.
         BRAND_NAME:   [
-            CallbackQueryHandler(get_filter(step=0), pass_chat_data=True),
+            CallbackQueryHandler(filter(by="brand_name"), pass_chat_data=True),
             CommandHandler("cancel", cancel)
         ],
         PRODUCT_NAME: [
-            CallbackQueryHandler(get_filter(step=1), pass_chat_data=True),
+            CallbackQueryHandler(filter(by="name"), pass_chat_data=True),
             CommandHandler("cancel", cancel)
         ],
         UOM:          [
-            CallbackQueryHandler(get_filter(step=2), pass_chat_data=True),
+            CallbackQueryHandler(filter(by="uom"), pass_chat_data=True),
             CommandHandler("cancel", cancel)
         ],
         SHOP:         [
-            CallbackQueryHandler(get_filter(step=3), pass_chat_data=True),
+            CallbackQueryHandler(filter(by="shop"), pass_chat_data=True),
             CommandHandler("cancel", cancel)
         ],
         # Choose action.
